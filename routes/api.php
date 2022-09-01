d9d570e5bb51cb9773b0a0b6adb8d1cc9b33abb2
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-01 08:38:22 -0400

    Test route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'v1'
+], function () {
+    Route::post("test", function () {
+        return response()->json([
+            'Hello World :D!'
+        ]);
+    });
 });
